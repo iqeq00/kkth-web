077b045b2157500d33415dde8f249424552c1f37
iqeq00 <iqeq00@gmail.com>
2019-11-08 15:52:08 +0800

    Modify background photo

--- src/main/resources/static/js/login/bgStretcher.js
+++ src/main/resources/static/js/login/bgStretcher.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.login-wrapper').bgStretcher({
-        images: ['/img/bg/01.jpg', '/img/bg/02.jpg', '/img/bg/03.jpg', '/img/bg/04.jpg', '/img/bg/05.jpg'],
+        images: ['/img/bg/01.jpg', '/img/bg/02.jpg', '/img/bg/03.jpg', '/img/bg/04.jpg'],
         imageWidth: 1920,
         imageHeight: 1080,
         nextSlideDelay: 5000,
