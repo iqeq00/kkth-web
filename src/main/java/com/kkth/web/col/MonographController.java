10431cb4ad4448770d98e0e419df1a4afe25a1ba
iqeq00 <iqeq00@gmail.com>
2019-11-12 17:17:35 +0800

    Add monograph module

--- src/main/java/com/kkth/web/col/MonographController.java
+++ src/main/java/com/kkth/web/col/MonographController.java
@@ -1,9 +1,18 @@
 package com.kkth.web.col;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.additional.query.impl.LambdaQueryChainWrapper;
+import com.kkth.framework.annotations.ApiOperation;
+import com.kkth.framework.annotations.Resources;
 import com.kkth.framework.controller.SuperController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kkth.framework.responses.ApiResponses;
+import com.kkth.web.model.entity.Monograph;
+import com.kkth.web.service.MonographService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -17,4 +26,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/monograph")
 public class MonographController extends SuperController {
 
+    @Autowired
+    private MonographService monographService;
+
+    @Resources
+    @ApiOperation("专著查询(分页)")
+    @GetMapping("/page")
+    public ApiResponses<IPage<Monograph>> page(
+            @RequestParam(value = "name", defaultValue = "") String name) {
+
+        LambdaQueryChainWrapper<Monograph> qw = monographService.lambdaQuery();
+        if (StringUtils.isNotBlank(name)) {
+            qw.like(Monograph::getName, name);
+        }
+        return success(qw.page(this.<Monograph>getPage()));
+    }
+
+    @Resources
+    @ApiOperation("专著添加")
+    @PostMapping
+    public ApiResponses<Void> save(@RequestBody Monograph monograph) {
+
+        monographService.save(monograph);
+        return success(HttpStatus.CREATED);
+    }
+
+    @Resources
+    @ApiOperation("专著编辑")
+    @PutMapping("/{id}")
+    public ApiResponses<Void> update(@PathVariable("id") Integer id, @RequestBody Monograph monograph) {
+
+        monograph.setId(id);
+        monographService.updateById(monograph);
+        return success();
+    }
+
+    @Resources
+    @ApiOperation("专著删除")
+    @DeleteMapping("/{id}")
+    public ApiResponses<Void> delete(@PathVariable("id") Integer id) {
+
+        monographService.removeById(id);
+        return success(HttpStatus.NO_CONTENT);
+    }
 }
