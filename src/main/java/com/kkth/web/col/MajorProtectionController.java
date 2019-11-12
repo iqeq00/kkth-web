54086ffbdcd92ce5ff045b35ce793cda2f711374
iqeq00 <iqeq00@gmail.com>
2019-11-12 16:56:50 +0800

    Add major protection module and repair layui form label 4 word question

--- src/main/java/com/kkth/web/col/MajorProtectionController.java
+++ src/main/java/com/kkth/web/col/MajorProtectionController.java
@@ -1,9 +1,18 @@
 package com.kkth.web.col;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.additional.query.impl.LambdaQueryChainWrapper;
+import com.kkth.framework.annotations.ApiOperation;
+import com.kkth.framework.annotations.Resources;
 import com.kkth.framework.controller.SuperController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kkth.framework.responses.ApiResponses;
+import com.kkth.web.model.entity.MajorProtection;
+import com.kkth.web.service.MajorProtectionService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,7 +23,50 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-11-12
  */
 @RestController
-@RequestMapping("/major-protection")
+@RequestMapping("/majorProtection")
 public class MajorProtectionController extends SuperController {
 
+    @Autowired
+    private MajorProtectionService majorProtectionService;
+
+    @Resources
+    @ApiOperation("重点保护对象查询(分页)")
+    @GetMapping("/page")
+    public ApiResponses<IPage<MajorProtection>> page(
+            @RequestParam(value = "name", defaultValue = "") String name) {
+
+        LambdaQueryChainWrapper<MajorProtection> qw = majorProtectionService.lambdaQuery();
+        if (StringUtils.isNotBlank(name)) {
+            qw.like(MajorProtection::getName, name);
+        }
+        return success(qw.page(this.<MajorProtection>getPage()));
+    }
+
+    @Resources
+    @ApiOperation("重点保护对象添加")
+    @PostMapping
+    public ApiResponses<Void> save(@RequestBody MajorProtection majorProtection) {
+
+        majorProtectionService.save(majorProtection);
+        return success(HttpStatus.CREATED);
+    }
+
+    @Resources
+    @ApiOperation("重点保护对象编辑")
+    @PutMapping("/{id}")
+    public ApiResponses<Void> update(@PathVariable("id") Integer id, @RequestBody MajorProtection majorProtection) {
+
+        majorProtection.setId(id);
+        majorProtectionService.updateById(majorProtection);
+        return success();
+    }
+
+    @Resources
+    @ApiOperation("重点保护对象删除")
+    @DeleteMapping("/{id}")
+    public ApiResponses<Void> delete(@PathVariable("id") Integer id) {
+
+        majorProtectionService.removeById(id);
+        return success(HttpStatus.NO_CONTENT);
+    }
 }
---
+++ src/main/resources/static/js/pkmanage/majorProtection.js
@@ -0,0 +1,123 @@
+layui.use(['config', 'lichee', 'jquery', 'layer', 'table', 'form', 'imageUpload'], function () {
+
+    var config = layui.config;
+    var lichee = layui.lichee;
+    var $ = layui.jquery;
+    var layer = layui.layer;
+    var table = layui.table;
+    var form = layui.form;
+    var imageUpload = layui.imageUpload;
+
+    var tableInfo = table.render({
+        elem: '#table',
+        toolbar: '#toolbar',
+        defaultToolbar: [],
+        url: '/majorProtection/page',
+        title: '重点保护对象',
+        page: true,
+        headers: {Authorization: config.getToken()},
+        request: config.request,
+        parseData: config.parseData,
+        response: config.response,
+        cols: [[
+            {field: 'parkId', align: 'center', sort: true, title: '地质公园ID'},
+            {field: 'protectionNumber', align: 'center', sort: true, title: '保护区编号'},
+            {field: 'protectionObjectNumber', align: 'center', sort: true, title: '保护对象编号'},
+            {field: 'name', align: 'center', sort: true, title: '名称'},
+            {field: 'protectionObjectCategory', align: 'center', sort: true, title: '保护对象类型'},
+            {field: 'geologicalAge', align: 'center', sort: true, title: '地质年代'},
+            // {field: 'safeguard', align: 'center', sort: true, title: '保护措施'},
+            {field: 'lengths', align: 'center', sort: true, title: '长度'},
+            {field: 'acreage', align: 'center', sort: true, title: '面积'},
+            // {field: 'protectionLevel', align: 'center', sort: true, title: '保护级别'},
+            {fixed: 'right',align : 'center', title: '操作', toolbar: '#bar', width: 180}
+        ]]
+    });
+
+    table.on('toolbar(tableFilter)', function (obj) {
+        var checkStatus = table.checkStatus(obj.config.id);
+        switch (obj.event) {
+            case 'add':
+                showEditModel();
+                break;
+            case 'getCheckLength':
+                var data = checkStatus.data;
+                layer.msg('选中了：' + data.length + ' 个');
+                break;
+            case 'isAll':
+                layer.msg(checkStatus.isAll ? '全选' : '未全选');
+                break;
+        };
+    });
+
+    table.on('tool(tableFilter)', function (obj) {
+        var data = obj.data;
+        if (obj.event === 'del') {
+            layer.confirm('确定要删除吗？', function (index) {
+                layer.load(2);
+                lichee.delete('/majorProtection/' + obj.data.id, {}, function () {
+                    layer.closeAll('loading');
+                    layer.msg('删除成功', {icon: 1});
+                    obj.del();
+                });
+            });
+        } else if (obj.event === 'edit') {
+            showEditModel(data);
+        }
+    });
+
+    var showEditModel = function (data) {
+        layer.open({
+            title: data ? '详情/修改' : '添加',
+            type: 1,
+            area: ['800px', '560px'],
+            content: $('#form-model').html(),
+            success: function () {
+                $('#form')[0].reset();
+                lichee.get('/park/list', {async: false}, function (data) {
+                    $('#parkId').vm({parks: data.result});
+                    form.render('select');
+                });
+                imageUpload.init("uploadBtn", "img", "majorProtection");
+                if (data) {
+                    form.val('formFilter', data);
+                    imageUpload.initImageList(data.img);
+                }
+                $('#form .close').click(function () {
+                    layer.closeAll('page');
+                });
+            }
+        });
+    };
+
+    var form = layui.form;
+    form.on('submit(formSubmit)', function (data) {
+        layer.load(2);
+        if (data.field.id) {
+            lichee.put('/majorProtection/' + data.field.id, {data: data.field}, function (res) {
+                callFunction(res);
+            });
+        } else {
+            lichee.post('/majorProtection', {data: data.field}, function (res) {
+                callFunction(res);
+            });
+        }
+        return false;
+    });
+
+    var callFunction = function (res) {
+        layer.closeAll();
+        if (res.status == 200) {
+            layer.msg("修改成功",{icon: 1});
+            tableInfo.reload({where: lichee.getSearchForm()});
+        } else if(res.status == 201)  {
+            layer.msg("添加成功",{icon: 1});
+            tableInfo.reload({where: lichee.getSearchForm()});
+        }
+    };
+
+    $('#searchButton').click(function () {
+        tableInfo.reload({where: lichee.getSearchForm()});
+    });
+
+});
